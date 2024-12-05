a63bfd6a5d50f46800b07ca8339b6b50ac9c7e58
Arnoldas Skinderis <102791583+AScotM@users.noreply.github.com>
2024-12-05 16:01:57 +0200

    Update rand_pass.py

--- rand_pass.py
+++ rand_pass.py
@@ -1,11 +1,10 @@
-import random
 import string
+import secrets
 
 def generate_random_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for i in range(length))
+    password = ''.join(secrets.choice(characters) for i in range(length))
     return password
 
 random_password = generate_random_password(12)
 print("Randomly generated password:", random_password)
-
